49fca030bd396ee61b2d141c4d3b56704aae23ac
msoler75 <msoler75@yahoo.es>
2021-08-27 13:19:45 -0400

    extra field

--- api/novedades/controllers/novedades.js
+++ api/novedades/controllers/novedades.js
@@ -26,9 +26,13 @@ module.exports = {
             noticias: 'titular'
         }
 
+        const cextra = {
+            eventos: 'fechaComienzo'
+        }
+
         const sql = tipo ?
         `
-        SELECT ${tipo}.id, slug, ${ctitulo[tipo] || 'titulo'}, ${cdescripcion[tipo] || 'descripcion'}, ${tipo}.updated_at, "${tipo}" AS tipo, uf.url as imagen, uf.width, uf.height FROM ${tipo} 
+        SELECT ${tipo}.id, slug, ${ctitulo[tipo] || 'titulo'}, ${cdescripcion[tipo] || 'descripcion'}, ${tipo}.updated_at, "${tipo}" AS tipo, uf.url as imagen, uf.width, uf.height, ${cextra[tipo] || "''"} as extra FROM ${tipo} 
         JOIN upload_file_morph ufm ON related_id=${tipo}.id AND related_type="${tipo}" AND field="imagen"
         JOIN upload_file uf ON upload_file_id=uf.id
         ${updated_at?"WHERE "+tipo+".updated_at<'"+updated_at+"'":""}
@@ -37,23 +41,23 @@ module.exports = {
         `
         :
         `
-        SELECT entradas.id, slug, ${ctitulo['entradas'] || 'titulo'}, ${cdescripcion['entradas'] || 'descripcion'}, entradas.updated_at, "entradas" AS tipo, uf.url as imagen, uf.width, uf.height  FROM entradas 
+        SELECT entradas.id, slug, ${ctitulo['entradas'] || 'titulo'}, ${cdescripcion['entradas'] || 'descripcion'}, entradas.updated_at, "entradas" AS tipo, uf.url as imagen, uf.width, uf.height, '' as extra  FROM entradas 
             JOIN upload_file_morph ufm ON related_id=entradas.id AND related_type="entradas" AND field="imagen"
             JOIN upload_file uf ON upload_file_id=uf.id
         UNION
-        SELECT noticias.id, slug, ${ctitulo['noticias'] || 'titulo'}, ${cdescripcion['noticias'] || 'descripcion'}, noticias.updated_at, "noticias" AS tipo, uf.url as imagen, uf.width, uf.height FROM noticias 
+        SELECT noticias.id, slug, ${ctitulo['noticias'] || 'titulo'}, ${cdescripcion['noticias'] || 'descripcion'}, noticias.updated_at, "noticias" AS tipo, uf.url as imagen, uf.width, uf.height, '' as extra FROM noticias 
             JOIN upload_file_morph ufm ON related_id=noticias.id AND related_type="noticias" AND field="imagen"
             JOIN upload_file uf ON upload_file_id=uf.id
         UNION
-        SELECT comunicados.id, slug, ${ctitulo['comunicados'] || 'titulo'}, ${cdescripcion['comunicados'] || 'descripcion'}, comunicados.updated_at, "comunicados" AS tipo, uf.url as imagen, uf.width, uf.height  FROM comunicados 
+        SELECT comunicados.id, slug, ${ctitulo['comunicados'] || 'titulo'}, ${cdescripcion['comunicados'] || 'descripcion'}, comunicados.updated_at, "comunicados" AS tipo, uf.url as imagen, uf.width, uf.height, '' as extra  FROM comunicados 
             JOIN upload_file_morph ufm ON related_id=comunicados.id AND related_type="comunicados" AND field="imagen"
             JOIN upload_file uf ON upload_file_id=uf.id
         UNION
-        SELECT eventos.id, slug, ${ctitulo['eventos'] || 'titulo'}, ${cdescripcion['eventos'] || 'descripcion'}, eventos.updated_at, "eventos" AS tipo, uf.url as imagen, uf.width, uf.height  FROM eventos 
+        SELECT eventos.id, slug, ${ctitulo['eventos'] || 'titulo'}, ${cdescripcion['eventos'] || 'descripcion'}, eventos.updated_at, "eventos" AS tipo, uf.url as imagen, uf.width, uf.height, fechaComienzo as extra  FROM eventos 
             JOIN upload_file_morph ufm ON related_id=eventos.id AND related_type="eventos" AND field="imagen"
             JOIN upload_file uf ON upload_file_id=uf.id
         UNION
-        SELECT libros.id, slug, ${ctitulo['libros'] || 'titulo'}, ${cdescripcion['libros'] || 'descripcion'}, libros.updated_at, "libros" AS tipo, uf.url as imagen, uf.width, uf.height  FROM libros 
+        SELECT libros.id, slug, ${ctitulo['libros'] || 'titulo'}, ${cdescripcion['libros'] || 'descripcion'}, libros.updated_at, "libros" AS tipo, uf.url as imagen, uf.width, uf.height, '' as extra  FROM libros 
             JOIN upload_file_morph ufm ON related_id=libros.id AND related_type="libros" AND field="imagen"
             JOIN upload_file uf ON upload_file_id=uf.id
         ORDER BY updated_at DESC
