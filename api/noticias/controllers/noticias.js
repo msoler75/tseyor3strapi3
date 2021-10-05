e044bf9c1abea51c33f0dc20037dcf87a36001eb
msoler75 <msoler75@yahoo.es>
2021-10-05 13:00:13 -0300

    likes

--- api/noticias/controllers/noticias.js
+++ api/noticias/controllers/noticias.js
@@ -1,8 +1,22 @@
-'use strict';
+const likescontroller = require('../../likeslib/controllers.js')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+      // https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#controllers
+
+    async like(ctx) {
+        return likescontroller.like('noticias', ctx)
+    },
+  
+  
+    
+    async dislike(ctx) {
+      return likescontroller.dislike('noticias', ctx)
+    },
+
+};
