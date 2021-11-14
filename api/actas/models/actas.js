9e9adabe0a79d7b9e94b2dd24568daa45fa7e4d4
msoler75 <msoler75@yahoo.es>
2021-11-14 17:35:34 -0300

    mejoras model actas

--- api/actas/models/actas.js
+++ api/actas/models/actas.js
@@ -1,8 +1,45 @@
-'use strict';
+'use strict'
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+// sirve para normalizar la información relacional
+async function normalizeData (data) {
+  const reunion = data.reunion
+    ? await strapi.services.reuniones.findOne({ id: data.reunion })
+    : null
+  const actividad = reunion
+    ? await strapi.services.reuniones.findOne({ id: reunion.actividad.id })
+    : data.actividad
+    ? await strapi.services.actividades.findOne({ id: data.actividad })
+    : data.equipo
+    ? await strapi.services.actividades.findOne({
+        titulo: 'Reuniones',
+        equipo: data.equipo
+      })
+    : null
+  if (data.reunion) {
+    data.actividad = reunion.actividad.id
+    data.equipo = reunion.equipo.id
+  } else if (data.actividad) {
+    data.reunion = null
+    data.equipo = actividad.equipo.id
+  } else if (data.equipo) {
+    data.reunion = null
+    data.actividad = null
+  }
+}
+
+module.exports = {
+  lifecycles: {
+    async beforeCreate (data) {
+      await normalizeData(data)
+    },
+
+    async beforeUpdate (params, data) {
+      await normalizeData(data)
+    }
+  }
+}
